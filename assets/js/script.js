da549810f80c0d415ffdb43abe7d05d98fc6f0c7
sams4566 <sams4566@gmail.com>
2021-09-27 10:01:02 +0000

    Set up the computer selection and play selection functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,7 @@
-let buttons = document.getElementsByTagName("button")
+let buttons = document.getElementsByTagName("button");
+let selection = ["rock", "paper", "scissors", "lizard", "spock"];
+let userImage = document.getElementById("user-selection-image");
+let computerImage = document.getElementById("computer-selection-image");
 
 for (let button of buttons) {
     button.addEventListener("click", function() {
@@ -7,5 +10,15 @@ for (let button of buttons) {
     })
 
 function runGame(userSelection) {
+    userImage.src = `assets/images/${selection[userSelection]}.jpg`;
+    userImage.alt = selection[userSelection];
 
+    let computerSelection = Math.floor(Math.random() * 5);
+
+    computerImage.src = `assets/images/${selection[computerSelection]}.jpg`;
+    computerImage.alt = selection[computerSelection]
+
+    let result = findWinner(selection[userSelection], selection[computerSelection])
+
+    points(result)
 }
