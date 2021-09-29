56197e5e3235f5c4841649d042b6e60ed68af7b9
sams4566 <sams4566@gmail.com>
2021-09-29 14:44:24 +0000

    Add notes to the java script to make it easier to understand

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,11 @@
+/*jshint esversion: 6 */
+
+/**
+ * Starts off by removing the game options when the user
+ * clicks on their choice. The code then splits off into
+ * two streams, one for each game.
+ */
+
 document.addEventListener("DOMContentLoaded", function() {
     let outOfFive = document.getElementById("outOfFive");
     let outOfTen = document.getElementById("outOfTen");
@@ -31,10 +39,20 @@ document.addEventListener("DOMContentLoaded", function() {
     }});
 });
 
+/**
+ * Message directing the user to click either rock, paper, scissors etc 
+ * after they have chosen their game option.
+ */
+
 function chooseMessage() {
     let chooseMessage = document.getElementById("info-message");
     chooseMessage.innerText = `Choose Below`;
 }
+
+/**
+ * Changes the image for the user and computers selections for the 
+ * 'First to 5' game
+ */
 
 function runGame1(userSelection1) {
     let selection = ["rock", "paper", "scissors", "lizard", "spock"];
@@ -54,6 +72,11 @@ function runGame1(userSelection1) {
     points1(result1);
 }
 
+/**
+ * Changes the image for the user and computers selections for the 
+ * 'First to 10' game
+ */
+
 function runGame2(userSelection2) {
     let selection = ["rock", "paper", "scissors", "lizard", "spock"];
     let userImage = document.getElementById("user-selection-image");
@@ -72,156 +95,10 @@ function runGame2(userSelection2) {
     points2(result2);
 }
 
-function points1(result1) {
-    if (result1 === "win") {
-        increaseUserScore1();
-    } else if (result1 === "lose") {
-        increaseComputerScore1();
-    } else if (result1 === "draw") {
-        drawMessage1();
-    } else {
-        alert(`Abort`);
-    }
-}
-
-function points2(result2) {
-    if (result2 === "win") {
-        increaseUserScore2();
-    } else if (result2 === "lose") {
-        increaseComputerScore2();
-    } else if (result2 === "draw") {
-        drawMessage2();
-    } else {
-        alert(`Abort`);
-    }
-}
-
-function increaseUserScore1() {
-    let currentScore1 = parseInt(document.getElementById("user-score").innerText);
-    let currentScore2 = parseInt(document.getElementById("computer-score").innerText);
-
-    let message = document.getElementById('info-message');
-    if (currentScore2 === 5) {
-        location.reload();
-    } else if (currentScore1 < 4) {
-        document.getElementById("user-score").innerText = ++currentScore1;
-        winMessage();
-    } else if (currentScore1 === 5) {
-        location.reload();
-    } else if (currentScore1 === 4) {
-        document.getElementById("user-score").innerText = ++currentScore1;
-        message.innerText = `You win!`;
-    } else {
-        alert(`Abort`);
-    }
-}
-
-function increaseComputerScore1() {
-    let currentScore1 = parseInt(document.getElementById("user-score").innerText);
-    let currentScore2 = parseInt(document.getElementById("computer-score").innerText);
-
-    let message = document.getElementById('info-message');
-    
-    if (currentScore1 === 5) {
-        location.reload();
-    } else if (currentScore2 < 4) {
-        document.getElementById("computer-score").innerText = ++currentScore2;
-        loseMessage();
-    } else if (currentScore2 === 5) {
-        location.reload();
-    } else if (currentScore2 === 4) {
-        document.getElementById("computer-score").innerText = ++currentScore2;
-        message.innerText = `Computer Wins!`;
-    } else {
-        alert(`Abort`);
-    }
-}
-
-function drawMessage1() {
-    let message = document.getElementById('info-message');
-    let currentScore1 = parseInt(document.getElementById("user-score").innerText);
-    let currentScore2 = parseInt(document.getElementById("computer-score").innerText);
-
-    if (currentScore1 === 5) {
-        location.reload();
-    } else if (currentScore2 === 5) {
-        location.reload();
-    } else if (currentScore1 < 5) {
-        message.innerText = `Draw!`;
-    } else if (currentScore2 < 5) {
-        message.innerText = `Draw!`;
-    } else {
-        alert(`Abort`);
-    }
-}
-
-function increaseUserScore2() {
-    let currentScore1 = parseInt(document.getElementById("user-score").innerText);
-    let currentScore2 = parseInt(document.getElementById("computer-score").innerText);
-    let message = document.getElementById('info-message');
-
-    if (currentScore2 === 10) {
-        location.reload();
-    } else if (currentScore1 < 9) {
-        document.getElementById("user-score").innerText = ++currentScore1;
-        winMessage();
-    } else if (currentScore1 === 10) {
-        location.reload();
-    } else if (currentScore1 === 9) {
-        document.getElementById("user-score").innerText = ++currentScore1;
-        message.innerText = `You Win!`;
-    } else {
-        alert(`Abort`);
-    }
-}
-
-function increaseComputerScore2() {
-    let currentScore1 = parseInt(document.getElementById("user-score").innerText);
-    let currentScore2 = parseInt(document.getElementById("computer-score").innerText);
-    let message = document.getElementById('info-message');
-    
-    if (currentScore1 === 10) {
-        location.reload();
-    } else if (currentScore2 < 9) {
-        document.getElementById("computer-score").innerText = ++currentScore2;
-        loseMessage();
-    } else if (currentScore2 === 10) {
-        location.reload();
-    } else if (currentScore2 === 9) {
-        document.getElementById("computer-score").innerText = ++currentScore2;
-        message.innerText = `Computer Wins!`;
-    } else {
-        alert(`Abort`);
-    }
-}
-
-function drawMessage2() {
-    let message = document.getElementById('info-message');
-    let currentScore1 = parseInt(document.getElementById("user-score").innerText);
-    let currentScore2 = parseInt(document.getElementById("computer-score").innerText);
-
-    if (currentScore1 === 10) {
-        location.reload();
-    } else if (currentScore2 === 10) {
-        location.reload();
-    } else if (currentScore1 < 10) {
-        message.innerText = `Draw!`;
-    } else if (currentScore2 < 10) {
-        message.innerText = `Draw!`;
-    } else {
-        alert(`Abort`);
-    }
-}
-
-function winMessage() {
-    let message = document.getElementById('info-message');
-    message.innerText = `Well done!`;
-}
-
-function loseMessage() {
-    let message = document.getElementById('info-message');
-    message.innerText = `Unlucky`;
-}
+/**
+ * Works out whether the user or computer has won the point for
+ * both game options
+ */
 
 function findWinner(userChoice, computerChoice) {
     if (userChoice === "rock") {
@@ -294,4 +171,182 @@ function findWinner(userChoice, computerChoice) {
         } else {
             alert(`Abort`);
         }}
+}
+
+/**
+ * The result is processed through the below function which leads
+ * to separate functions for each game depending on the outcome
+ */
+
+function points1(result1) {
+    if (result1 === "win") {
+        increaseUserScore1();
+    } else if (result1 === "lose") {
+        increaseComputerScore1();
+    } else if (result1 === "draw") {
+        drawMessage1();
+    } else {
+        alert(`Abort`);
+    }
+}
+
+function points2(result2) {
+    if (result2 === "win") {
+        increaseUserScore2();
+    } else if (result2 === "lose") {
+        increaseComputerScore2();
+    } else if (result2 === "draw") {
+        drawMessage2();
+    } else {
+        alert(`Abort`);
+    }
+}
+
+/**
+ * The scores are increased for the 'First to 5' game depending on the players previous 
+ * score and the game is reloaded once a winner is established
+ */
+
+function increaseUserScore1() {
+    let currentScore1 = parseInt(document.getElementById("user-score").innerText);
+    let currentScore2 = parseInt(document.getElementById("computer-score").innerText);
+
+    let message = document.getElementById('info-message');
+    if (currentScore2 === 5) {
+        location.reload();
+    } else if (currentScore1 < 4) {
+        document.getElementById("user-score").innerText = ++currentScore1;
+        winMessage();
+    } else if (currentScore1 === 5) {
+        location.reload();
+    } else if (currentScore1 === 4) {
+        document.getElementById("user-score").innerText = ++currentScore1;
+        message.innerText = `You win!`;
+    } else {
+        alert(`Abort`);
+    }
+}
+
+function increaseComputerScore1() {
+    let currentScore1 = parseInt(document.getElementById("user-score").innerText);
+    let currentScore2 = parseInt(document.getElementById("computer-score").innerText);
+
+    let message = document.getElementById('info-message');
+    
+    if (currentScore1 === 5) {
+        location.reload();
+    } else if (currentScore2 < 4) {
+        document.getElementById("computer-score").innerText = ++currentScore2;
+        loseMessage();
+    } else if (currentScore2 === 5) {
+        location.reload();
+    } else if (currentScore2 === 4) {
+        document.getElementById("computer-score").innerText = ++currentScore2;
+        message.innerText = `Computer Wins!`;
+    } else {
+        alert(`Abort`);
+    }
+}
+
+/**
+ * If the result is a draw for the 'First to 5' game a message is displayed highlighting this 
+ */
+
+function drawMessage1() {
+    let message = document.getElementById('info-message');
+    let currentScore1 = parseInt(document.getElementById("user-score").innerText);
+    let currentScore2 = parseInt(document.getElementById("computer-score").innerText);
+
+    if (currentScore1 === 5) {
+        location.reload();
+    } else if (currentScore2 === 5) {
+        location.reload();
+    } else if (currentScore1 < 5) {
+        message.innerText = `Draw!`;
+    } else if (currentScore2 < 5) {
+        message.innerText = `Draw!`;
+    } else {
+        alert(`Abort`);
+    }
+}
+
+/**
+ * The scores are increased for the 'First to 10' game depending on the players previous 
+ * score and the game is reloaded once a winner is established
+ */
+
+function increaseUserScore2() {
+    let currentScore1 = parseInt(document.getElementById("user-score").innerText);
+    let currentScore2 = parseInt(document.getElementById("computer-score").innerText);
+    let message = document.getElementById('info-message');
+
+    if (currentScore2 === 10) {
+        location.reload();
+    } else if (currentScore1 < 9) {
+        document.getElementById("user-score").innerText = ++currentScore1;
+        winMessage();
+    } else if (currentScore1 === 10) {
+        location.reload();
+    } else if (currentScore1 === 9) {
+        document.getElementById("user-score").innerText = ++currentScore1;
+        message.innerText = `You Win!`;
+    } else {
+        alert(`Abort`);
+    }
+}
+
+function increaseComputerScore2() {
+    let currentScore1 = parseInt(document.getElementById("user-score").innerText);
+    let currentScore2 = parseInt(document.getElementById("computer-score").innerText);
+    let message = document.getElementById('info-message');
+    
+    if (currentScore1 === 10) {
+        location.reload();
+    } else if (currentScore2 < 9) {
+        document.getElementById("computer-score").innerText = ++currentScore2;
+        loseMessage();
+    } else if (currentScore2 === 10) {
+        location.reload();
+    } else if (currentScore2 === 9) {
+        document.getElementById("computer-score").innerText = ++currentScore2;
+        message.innerText = `Computer Wins!`;
+    } else {
+        alert(`Abort`);
+    }
+}
+
+/**
+ * If the result is a draw for the 'First to 10' game a message is displayed highlighting this 
+ */
+
+function drawMessage2() {
+    let message = document.getElementById('info-message');
+    let currentScore1 = parseInt(document.getElementById("user-score").innerText);
+    let currentScore2 = parseInt(document.getElementById("computer-score").innerText);
+
+    if (currentScore1 === 10) {
+        location.reload();
+    } else if (currentScore2 === 10) {
+        location.reload();
+    } else if (currentScore1 < 10) {
+        message.innerText = `Draw!`;
+    } else if (currentScore2 < 10) {
+        message.innerText = `Draw!`;
+    } else {
+        alert(`Abort`);
+    }
+}
+
+/**
+ * Functions for when the user wins or loses a point
+ */
+
+function winMessage() {
+    let message = document.getElementById('info-message');
+    message.innerText = `Well done!`;
+}
+
+function loseMessage() {
+    let message = document.getElementById('info-message');
+    message.innerText = `Unlucky`;
 }
